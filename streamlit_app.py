fd82ae9e540e7aaab1d1f0374acc233bbaa07e2a
aodhanlutetiae <39188450+aodhanlutetiae@users.noreply.github.com>
2024-06-14 23:25:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,14 @@ import requests
 st.title('Geiriau: words')
 st.subheader("What they talk about in the Welsh parliament")
 
-# import json
-# with open('year_WL.json', 'r') as file:
-#     d = json.load(file)
-    
 @st.cache_data
 def load_data(url):
     req = requests.get(url)
     return req.json()
 
 aws_l = 'https://seneddbucket.s3.amazonaws.com/year_WL.json'
-d = load_data(aws_l)
+with st.spinner(text="Hang on, we're loading up nine years of politicians talking..."):
+    d = load_data(aws_l)
 
 user_input = st.text_input("Look up a word", 'donation')
 f = open("log_terms.txt", "a")
@@ -30,12 +27,8 @@ for y in d:
     perc = (term_count / total_words) * 100
     search_dict[y] = perc
 
-# for k, v in search_dict.items():
-#     print(k, v)
-
 df = pd.Series(search_dict).to_frame().reset_index()
 df.columns = ['year', 'value']
-
 df2 = df.reset_index().set_index('year')['value']
 
 st.line_chart(df2)
