4a02979c66aab81c59899ddfc3334f452df3fa4f
aodhanlutetiae <39188450+aodhanlutetiae@users.noreply.github.com>
2024-07-12 23:16:23 +0100

    version 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,25 @@
-import pandas as pd
+import json
 import streamlit as st
-import requests
 import re
 
 # text
 st.title('Geiriau | Words')
 st.subheader("What they talk about in the Welsh parliament")
-
-# define function that loads the data
-@st.cache_data(max_entries = 1)
-def load_data(url):
-    req = requests.get(url)
-    return req.json()
-
-# use the function with AWS as address: 214 MB. Run waiting message
-aws_l = 'https://seneddbucket.s3.amazonaws.com/year_WL.json'
-aws_l_test = 'https://seneddbuckettest.s3.amazonaws.com/test_data_100k_10MB.json'
-
-with st.spinner(text="Hang on, we're loading up nine years of politicians talking..."):
-    d = load_data(aws_l_test)
+    
+# @st.cache_data(max_entries = 1)
+with open('year_FL.json') as json_data:
+    d = json.load(json_data)
 
 # collect user input and cast to lowercase
 user_input = st.text_input("Look up a word", 'donation').lower()
-
-# work through each year(key) in the dictionary to get len of wordlist(value) 
-# and to count occurences of word queried by user. Then add results to new dict
+ 
 search_dict = {}
 for y in d:
-    total_words = len(d[y])
-    term_count = len([x for x in d[y] if x == user_input])
-    perc = (term_count / total_words) * 100
-    search_dict[y] = perc
-
-# make a df from the dictionary just assembled
-df = pd.DataFrame.from_dict(search_dict, orient='index', columns=['value'])
+    result = next((v[1] for v in d[y] if v[0] == user_input), None)
+    search_dict[y] = result
 
 # make line chart
-st.line_chart(df)
+st.line_chart(search_dict)
 
 # extract the most recent update from the log file and print for the 'Last updated' line
 with open('repo_log.txt') as f:
